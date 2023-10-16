813fe754e942c8922944251c962be7291c519bca
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-10-15 23:09:22 -0400

    Added attributes to the Course class and the getters and seeter for each attribute.

--- Lab5Activity/Lab5Activity/Models/Course.cs
+++ Lab5Activity/Lab5Activity/Models/Course.cs
@@ -1,6 +1,14 @@
-﻿namespace Lab5Activity.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab5Activity.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
